9e45d31bff006d58524f0eb5e7a919820ce445f0
w_sh1t <wh1tesh1t.mail@gmail.com>
2025-07-05 09:56:07 +0300

    Deleted test sh1t

--- install-valve.sh
+++
@@ -1,9 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/env sh
-
-DUSERNAME="username"
-DPASSWORD="77817282"
-DAPP_ID="70"
-DDEPOT_ID="71"
-DGAMEDIR="valve"
-
-depotdownloader -username $DUSERNAME -password $DPASSWORD -app $DAPP_ID -depot $DDEPOT_ID -dir $DGAMEDIR -validate
